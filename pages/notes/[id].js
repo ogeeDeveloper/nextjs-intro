444b6d117abc777dbdb0189fa81958c2b54012d2
ogeeDeveloper <omarogrant202@hotmail.com>
2021-12-18 18:54:33 -0500

    Added routing avigation on the client side

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -5,7 +5,12 @@ const Page = () => {
   const router = useRouter()
   // Get the ID like so
   const { id } = router.query
-  return <div>Note #{id}</div>
+  return (
+    <div>
+      {/* The id will be printed on the page beside the Note */}
+      <h1>Note #{id}</h1>
+    </div>
+  )
 }
 
 export default Page
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link"
 const Notes = () => {
-  return <div>Notes index page</div>
+  return (
+    <div>
+      <h1>Notes index page</h1>
+      <Link href='/notes/[id]' as={`notes/1`}>
+        <a>Note 1</a>
+      </Link>
+    </div>
+  )
 }
 
 export default Notes
